fd486358b575bdcd90ebdcc09179bd1a78f12e2e
perepeluknastia1507@gmail.com <perepeluknastia1507@gmail.com>
2026-08-07 20:58:55 +0300

    fix built bugs

--- components/CreateNoteClient/CreateNoteClient.tsx
+++ components/CreateNoteClient/CreateNoteClient.tsx
@@ -2,7 +2,7 @@
 
 import { useRouter } from 'next/navigation';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { createNote } from '@/lib/api';
+import { createNote } from '@/lib/api/clientApi';
 import { useNoteStore } from '@/lib/store/noteStore';
 import { NoteForm } from '@/components/NoteForm/NoteForm';
 import toast from 'react-hot-toast';
